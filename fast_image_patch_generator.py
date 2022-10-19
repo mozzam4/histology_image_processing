2e782add17e7123cb6136425ec17c4fa98d2fc48
Mozzam Motiwala <mozzam4@gmail.com>
2022-10-19 14:57:26 +0200

    updated patch generator with canny checks and generating random 500 patches per image

--- fast_image_patch_generator.py
+++ fast_image_patch_generator.py
@@ -1,22 +1,48 @@
 import fast
 import numpy as np
-from skimage import io
+import os
+from scipy import ndimage as ndi
+from skimage import io, feature, color
+import random
 
-#  Set the input file path and the output Folder path
-input_file_path = r'C:\Users\mozza\Documents\test\M166A_issues2.ndpi'
-output_folder_path = r'C:\Users\mozza\Documents\test\patches\M166A_issues2'
+dir = r'C:\Users\mozza\Documents\test\Few_patches\selected_images'
+output_dir = r'C:\Users\mozza\Documents\test\Few_patches\selected_patches_small'
+for filename in os.listdir(dir):
 
-importer = fast.WholeSlideImageImporter.create(input_file_path)
 
-tissueSegmentation = fast.TissueSegmentation.create().connect(importer)
 
-patchGenerator = fast.PatchGenerator.create(1024, 1024, level=0)\
-    .connect(0, importer)\
-    .connect(1, tissueSegmentation)
+    input_file_path = os.path.join(dir, filename)
+    output_file_dir = os.path.join(output_dir, filename)
+    if not os.path.exists(output_file_dir):
+        os.mkdir(output_file_dir)
+        importer = fast.WholeSlideImageImporter.create(input_file_path)
+        tissueSegmentation = fast.TissueSegmentation.create().connect(importer)
+        patchGenerator = fast.PatchGenerator.create(512, 512, level=0)\
+            .connect(0, importer)\
+            .connect(1, tissueSegmentation)
 
-count = 0
-for patch in fast.DataStream(patchGenerator):
-    path_to_save_patch = output_folder_path + '\ ' + str(count) + '.jpg'
-    patch = np.asanyarray(patch)
-    io.imsave(path_to_save_patch, patch)
-    count = count + 1
+        count = 0
+        patch_list = []
+        processed_patches = []
+        for patch in fast.DataStream(patchGenerator):
+
+            patch_list.append(patch)
+        while count < 500:
+            random_patch = random.randint(0, len(patch_list) - 1)
+            if random_patch in processed_patches:
+                continue
+            # check if the patch fulfils the reqt
+            img = np.asanyarray(patch_list[random_patch])
+            img_gray = color.rgb2gray(img)
+            img_blur = ndi.gaussian_filter(img_gray, 3)
+            edges1 = feature.canny(img_blur, sigma=1)
+            unique, counts = np.unique(edges1, return_counts=True)
+            edge_to_image_ratio = counts[1] / counts[0]
+            if edge_to_image_ratio < 0.01:
+                continue
+
+            processed_patches.append(random_patch)
+            count = count + 1
+            path_to_save_patch = output_file_dir + '\ ' + str(count) + '.jpg'
+            io.imsave(path_to_save_patch, img)
+            del img, img_gray, img_blur, edges1
