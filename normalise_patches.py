e747cac317c7596a2c0473561e1d88dfdf83f83e
Mozzam Motiwala <mozzam4@gmail.com>
2022-10-19 14:34:27 +0200

    Adding code to remove blur and background artifact patches

--- normalise_patches.py
+++ normalise_patches.py
@@ -1,13 +1,15 @@
+from scipy import ndimage as ndi
+import matplotlib.pyplot as plt
 import numpy as np
-from skimage import io
+from skimage import io, feature, color
 import itk
 import os
-reference_image_filename = r'C:\Users\mozza\Documents\test\Few_patches\patches\R021A_reference_clean\1254.jpg'
+reference_image_filename = r'/home/mozzam/Downloads/Ref.png'
 reference_image = itk.imread(reference_image_filename)
 #reference_reader = itk.ImageFileReader.New(FileName=reference_image_filename)
 
 
-dir = r'C:\Users\mozza\Documents\test\Few_patches\patches\M143A1_issues'
+dir = r'/home/mozzam/Documents/Few_patches/patches/R021A_reference_clean'
 for filename in os.listdir(dir):
     print(filename)
     input_filename = os.path.join(dir, filename)
@@ -29,14 +31,40 @@ for filename in os.listdir(dir):
     # except Exception as X:
     #     print(X)
 
-    with open(os.path.join(dir, filename), 'r') as f:
-        input_image = itk.imread(input_filename)
-        eager_normalized_image = itk.structure_preserving_color_normalization_filter(
-            input_image,
-            reference_image,
-            color_index_suppressed_by_hematoxylin=0,
-            color_index_suppressed_by_eosin=1,
-        )
-        dir_to_save = r'C:\Users\mozza\Documents\test\Few_patches\patches\M143A1_issues_normalised'
-        path_to_save = os.path.join(dir_to_save, filename)
-        itk.imwrite(eager_normalized_image, path_to_save)
+    with open(input_filename, 'r') as f:
+        # use canny ede detector to check if the file has any issues as per ECHLE2022 preprocessing
+
+        #Getting the canny edges
+        #threshold = 4
+        img = io.imread(input_filename)
+        img_gray = color.rgb2gray(img)
+            #img_gray[img_gray < threshold] = 0
+            #img_gray[img_gray >= threshold] = 255
+        img_blur = ndi.gaussian_filter(img_gray, 3)
+        edges1 = feature.canny(img_blur, sigma=1)
+        #edges2 = feature.canny(img_blur, sigma=4)
+
+        #io.imsave(r'/home/mozzam/Documents/Few_patches/patches/R021A_reference_clean/edges1.jpg', edges1)
+        #io.imsave(r'/home/mozzam/Documents/Few_patches/patches/R021A_reference_clean/edges2.jpg', edges2)
+
+        # edges = cv2.Canny(img_blur, threshold, 10)
+        # io.imshow(edges)
+        # plt.imshow(edges)
+
+        unique, counts = np.unique(edges1, return_counts=True)
+        edge_to_image_ratio = counts[1]/counts[0]
+        if edge_to_image_ratio < 0.01:
+            io.imsave(r'/home/mozzam/Documents/Few_patches/patches/discarded/' + filename + '.jpg', img)
+        else:
+            io.imsave(r'/home/mozzam/Documents/Few_patches/patches/accepted/' + filename + '.jpg', img)
+
+        # input_image = itk.imread(input_filename)
+        # eager_normalized_image = itk.structure_preserving_color_normalization_filter(
+        #     input_image,
+        #     reference_image,
+        #     color_index_suppressed_by_hematoxylin=0,
+        #     color_index_suppressed_by_eosin=1,
+        # )
+        # dir_to_save = r'C:\Users\mozza\Documents\test\Few_patches\patches\M143A1_issues_normalised'
+        # path_to_save = os.path.join(dir_to_save, filename)
+        # itk.imwrite(eager_normalized_image, path_to_save)
